b6adbe6461668d0cb3e4d462b0331cc479835407
Kimiaparand <120417079+Kimiaparand@users.noreply.github.com>
2023-05-05 17:38:36 +0430

    Added Githup repo Url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was codedd by Kimia Fatehi is{" "}
+        <a href="https://github.com/Kimiaparand/react-weather-app">
+          opened-source
+        </a>
+      </footer>
+    </div>
+  );
 }
