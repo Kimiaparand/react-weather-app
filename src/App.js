8b56e221e933b81859d1efb2cd0a285e189f26f4
Kimiaparand <120417079+Kimiaparand@users.noreply.github.com>
2023-05-05 23:18:57 +0430

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,13 +3,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was codedd by Kimia Fatehi is{" "}
-        <a href="https://github.com/Kimiaparand/react-weather-app">
-          opened-source
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was codedd by{" "}
+          <a
+            href="https://github.com/Kimiaparand/react-weather-app"
+            target="_balnk"
+          >
+            Kimia Fatehi
+          </a>{" "}
+          is{" "}
+          <a href="https://github.com/Kimiaparand/react-weather-app">
+            opened-source on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
